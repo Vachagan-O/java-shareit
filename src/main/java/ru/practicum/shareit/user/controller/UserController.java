a1a13af38cadba038afbc37d1738948c0cd62747
Vachagan <va4agan.oganesian@yandex.ru>
2023-06-15 11:14:02 +0300

    ТЗ #14. add-bookings.

--- src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDto;
-import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.service.UserService;
 
 import javax.validation.Valid;
